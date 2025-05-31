685605fadcc07ba6af606c01db7da8baac25d711
AzerAbdullazade <azerabdullazade2006@gmail.com>
2025-05-31 21:53:45 +0400

    Update script.py

--- script.py
+++ script.py
@@ -7,9 +7,9 @@ with open('xss_wordlist', 'r', encoding='utf-8') as f:
         encoded_word = urllib.parse.quote(word)  # URL ucun payloadi duzgun vezyete salmaq
         url = f'https://hucum_edilecek_seyfe/?param={encoded_word}'
 
-        try:
+        try:  #try bloku içində  xəta yarada biləcək kod yazılır
             response = requests.get(url)
             if word in response.text:
                 print(f"Mümkün XSS tapıldı: {word}")
-        except requests.exceptions.RequestException as e: #
+        except requests.exceptions.RequestException as e: #except bloku isə həmin xəta baş verərsə, onu tutub idarə edir, proqramın birdən-birə çökməsinin qarşısını alır.
             print(f"Xəta baş verdi: {e}")
