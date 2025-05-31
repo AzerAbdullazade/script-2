8dbc868d03ce309d4049de04de10b9839f1140dc
AzerAbdullazade <azerabdullazade2006@gmail.com>
2025-05-31 21:48:04 +0400

    Add files via upload
    xss payload yoxlamaq

---
+++ script.py
@@ -0,0 +1,15 @@
+import requests #http(s) kimi sorgular ucundur
+import urllib.parse #url ucundur
+
+with open('xss_wordlist', 'r', encoding='utf-8') as f:
+    for line in f:
+        word = line.strip()
+        encoded_word = urllib.parse.quote(word)  # URL ucun payloadi duzgun vezyete salmaq
+        url = f'https://hucum_edilecek_seyfe/?param={encoded_word}'
+
+        try:
+            response = requests.get(url)
+            if word in response.text:
+                print(f"Mümkün XSS tapıldı: {word}")
+        except requests.exceptions.RequestException as e: #
+            print(f"Xəta baş verdi: {e}")
